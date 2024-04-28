87a6eae961e2dca561b684effc302642ffa0fa25
Marius <tresormuashane@gmail.com>
2024-04-28 16:42:56 -0600

    kin

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-  terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.61.0"
-    }
-  }
-}
-
-provider "aws" {
-  
-  region = "us-east-1"
-}
-
 resource "aws_iam_group" "special" {
   name = "special"
 
@@ -27,17 +13,4 @@ blueprint_id = "ubuntu_18_04"
 bundle_id = "medium_1_0"
 name = "dev-server"
 user_data = " sudo apt-get update && sudo apt-get install -y apache2 && sudo systemctl start apache2 && sudo systemctl enable apache2 && echo '<h1>This is deployed by Marius</h1>' | sudo tee /var/www/html/index.html"
-  
 }
-
-output "instance1_ip" {
-  value = aws_lightsail_instance.custom.private_ip_address
-}
-
-output "instance_ip" {
-  value = aws_lightsail_instance.custom.public_ip_address
-}
-
-output "my-arn" {
-  value = aws_lightsail_instance.custom.arn
-} 
